91afe306e7152e8ae5bb86c6964d37671fb01431
Seb de Raadt <seb@dfd.co.nz>
2018-10-25 21:43:38 +1300

    API: First cut of temperature request and note update

---
+++ Api/Controllers/TemperaturesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Service.Entities;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using Api.Models;
+
+namespace Temperatures.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperaturesController : ControllerBase
+    {
+        private readonly EntitiesContext entities;
+
+        public TemperaturesController(EntitiesContext entities)
+        {
+            this.entities = entities;
+        }
+
+        // GET api/temperatures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Temperature>>> Get(CancellationToken cancellationToken)
+        {
+            var temperatures = await entities.Temperatures.Take(10).ToListAsync(cancellationToken);
+            return temperatures;
+        }
+
+        // POST api/temperatures/12345/note
+        [HttpPost("{id}/note")]
+        public async Task<ActionResult> Post([FromRoute] int id, [FromBody] UpdateNoteRequest model, CancellationToken cancellationToken)
+        {
+            var temperature = await entities.Temperatures.SingleOrDefaultAsync(t=>t.Id == id, cancellationToken);
+            if (temperature == null) return NotFound();
+            temperature.Note = model.Value;
+            await entities.SaveChangesAsync(cancellationToken);
+            return Ok();
+        }
+
+    }
+}
---
+++ Api/Models/UpdateNoteRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Api.Models
+{
+    public class UpdateNoteRequest
+    {
+        public string Value { get; set; }
+    }
+}
