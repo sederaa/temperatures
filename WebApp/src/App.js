023d1bab300e32ce67ef539fe08f4f75519ed6fa
Seb de Raadt <seb@dfd.co.nz>
2018-10-27 13:25:04 +1300

    Added table

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, Form, FormGroup, Label, InputGroup, InputGroupAddon } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, Form, FormGroup, Label, Table } from 'reactstrap';
 import classnames from 'classnames';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -15,7 +15,12 @@ class App extends Component {
     this.state = {
       activeTab: '1',
       startDate: moment().subtract(7, 'days'),
-      endDate: moment()
+      endDate: moment(),
+      temperatures: [
+        { When: moment(), ValueInCelsius: 23, Note: "Hello World!" },
+      { When: moment(), ValueInCelsius: 23, Note: "Hello World!" },
+      { When: moment(), ValueInCelsius: 23, Note: "Hello World!" }
+    ]
     };
     this.toggle = this.toggle.bind(this);
     this.handleDateChangeStart = this.handleDateChangeStart.bind(this);
@@ -48,8 +53,8 @@ class App extends Component {
         <h1>Temperatures</h1>
 
         <Form inline>
-          <FormGroup style={{marginRight:"1em"}}>
-            <Label for="startDate" style={{margin:"0.5em"}}>From</Label>
+          <FormGroup style={{ marginRight: "1em" }}>
+            <Label for="startDate" style={{ margin: "0.5em" }}>From</Label>
             <DatePicker
               id="startDate"
               selected={this.state.startDate}
@@ -61,7 +66,7 @@ class App extends Component {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="endDate" style={{margin:"0.5em"}}>To</Label>
+            <Label for="endDate" style={{ margin: "0.5em" }}>To</Label>
             <DatePicker
               id="endDate"
               selected={this.state.endDate}
@@ -97,25 +102,32 @@ class App extends Component {
           <TabPane tabId="1">
             <Row>
               <Col sm="12">
-                <h4>Tab 1 Contents</h4>
+                {this.state.temperatures.length > 0
+                  ?
+                  <Table striped hover size="sm" className="temperatures-table">
+                    <thead>
+                      <tr>
+                        <th>Date &amp; Time</th>
+                        <th>Value (°C)</th>
+                        <th>Notes</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.temperatures.map(t => <tr>
+                        <td>{t.When.fromNow()} ({t.When.format("D MMM YYYY h:mm:A")})</td>
+                        <td>{t.ValueInCelsius}</td>
+                        <td>{t.Notes}</td>
+                      </tr>)}
+                    </tbody>
+                  </Table>
+                  : <p>No temperatures found for this date range.</p>}
               </Col>
             </Row>
           </TabPane>
           <TabPane tabId="2">
             <Row>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
-              </Col>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
+              <Col sm="12">
+                Tab 2
               </Col>
             </Row>
           </TabPane>
